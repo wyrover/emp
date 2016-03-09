8cd962de19f4f823206b856ebd19aaba3f113e1c
Partoo <Partoo@users.noreply.github.com>
2016-03-09 17:04:50 +0800

    Update website URL

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="container-fluid footer">
-    版权所有 &copy; 2014-{{date('Y')}} <a href="http://staraw.com/" target="_blank">青岛世达奥科网络技术</a>
+    版权所有 &copy; 2014-{{date('Y')}} <a href="http://www.stario.com/" target="_blank">青岛世达奥科网络技术</a>
 </footer>
